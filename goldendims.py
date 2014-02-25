2912e0ffb162156e023f25856a5a90185e81b115
François Le Lay <mfworx@gmail.com>
2014-02-25 13:44:05 -0500

    more refactoring

--- goldendims.py
+++ goldendims.py
@@ -1,21 +1,21 @@
-import math
+from math import *
 
 class GoldenRectangle:
 
     def __init__(self, s, minRatio, maxRatio ):
         # golden number
-        self.r = (1+math.sqrt(5))/2
+        self.r = (1+sqrt(5))/2
         self.s = s
         self.minRatio = minRatio
         self.maxRatio = maxRatio
 
     def dimensions(self):
-        x0 = math.floor(math.sqrt(self.s * self.r))
+        x0 = floor(sqrt(self.s * self.r))
         return (self.xg(x0), self.xd(x0+1))
 
     def xg(self, a):
         ret = 0
-        if ( (self.s%a!=0) and ((a*a)/self.s>=self.minRatio) ):
+        if ( (fmod(self.s,a)!=0) and ((a*a)/self.s>=self.minRatio) ):
             ret = self.xg(a-1)
         else:
             if ((a*a)/self.s >= self.minRatio):
@@ -25,10 +25,11 @@ class GoldenRectangle:
 
     def xd(self, a):
         ret = 0
-        if ( (self.s%a!=0) and ((a*a)/self.s <= self.maxRatio) ):
+        if ( (fmod(self.s,a)!=0) and ((a*a)/self.s <= self.maxRatio) ):
             self.xd(a+1)
         else:
             if ((a*a)/ self.s <= self.maxRatio) :
                 ret = a
         return ret
 
+GoldenRectangle(1880,1.5,1.7).dimensions()

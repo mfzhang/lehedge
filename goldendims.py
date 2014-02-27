83f18f733440b3a85d79e47b9225f4621a768cc4
François Le Lay <mfworx@gmail.com>
2014-02-27 18:15:07 -0500

    golden search takes no constraints

--- goldendims.py
+++ goldendims.py
@@ -3,74 +3,62 @@ from math import *
 def notAnInteger(n):
     return fmod(n,1)!=0
 
-class GoldenRectangle:
+class GorightHitenRectangle:
 
-    def __init__(self, s, minRatio, maxRatio ):
-        # golden number
+    def __init__(self, s ):
+        # gorightHiten number
         self.r = (1+sqrt(5))/2
         self.s = float(s)
-        self.minRatio = minRatio
-        self.maxRatio = maxRatio
+        self.minRatio = 1
+        self.maxRatio = s
 
 
     def dimensions(self):
         x0 = int(sqrt(self.s * self.r))
         print "x0=%d" % x0
-        Lg = self.xg(x0)
-        # lg = self.s/Lg
-        # leftDiff = self.r-Lg/lg
-        Ld = self.xd(x0+1)
-        # ld = self.s/Ld
-        # rightDiff = Ld/ld-self.r
-        if ( Lg ==0 ):
-            if( Ld == 0):
+        leftHit = self.leftSearch(x0)
+        rightHit = self.rightSearch(x0+1)
+        if ( leftHit ==0 ):
+            if( rightHit == 0):
                 return 0
             else:
-                return Ld
-        elif (Ld == 0):
-            return Lg
+                return rightHit
+        elif (rightHit == 0):
+            return leftHit
         else:
-            lg = self.s/Lg
-            leftDiff = self.r-Lg/lg
-            ld = self.s/Ld
-            rightDiff = Ld/ld-self.r
+            leftOtherSide = self.s/leftHit
+            rightOtherSide = self.s/rightHit
+            if leftHit > leftOtherSide:
+                leftRatio = leftHit/leftOtherSide
+            else:
+                leftRatio = leftOtherSide/leftHit
+            if rightHit > rightOtherSide:
+                rightRatio = rightHit/rightOtherSide
+            else:
+                rightRatio = rightOtherSide/rightHit
+            leftDiff = abs(self.r-leftRatio)
+            rightDiff = abs(self.r-rightRatio)
+            print "Delta to golden number: left = %2f, right = %2f" % (leftDiff,rightDiff)
             if( leftDiff < rightDiff) :
-                return Lg
-            else: return Ld
+                return leftHit
+            else: return rightHit
 
-        #        elif (leftDiff < rightDiff):
-        #             return Lg
-        #         else:
-        #             return Ld
 
-    def xg(self, a):
-        print "a = %d" % notAnInteger(self.s/a)
+    def leftSearch(self, a):
+        print "a = %d" % (a)
         if ((a*a)/self.s >= self.minRatio ):
             if( notAnInteger(self.s/a) ):
-                return self.xg(a-1)
+                return self.leftSearch(a-1)
             else: return a
         else: return 0
 
-        #        if ( notAnInteger(self.s/a) and ((a*a)/self.s >= self.minRatio) ):
-        #             print "xg1 %d" % (a)
-        #             return self.xg(a-1)
-        #         elif ((a*a)/self.s >= self.minRatio):
-        #             print "xg2 %d" % (a)
-        #             return  a
-        #         else: return 0
 
-    def xd(self, a):
+    def rightSearch(self, a):
+        print "a = %d" % (a)
         if( (a*a)/self.s <= self.maxRatio ):
             if ( notAnInteger(self.s/a) ):
-                return self.xd(a+1)
+                return self.rightSearch(a+1)
             else: return a
         else: return 0
-        #        if ( notAnInteger(self.s/a) and ((a*a)/self.s <= self.maxRatio) ):
-        #            print "xd1 & %d" % (a)
-        #            return self.xd(a+1)
-        #        elif ((a*a)/ self.s <= self.maxRatio) :
-        #            print "xd2 # %d" % (a)
-        #            return a
-        #        else: return 0
 
-GoldenRectangle(1880,1.5,1.7).dimensions()
+GorightHitenRectangle(1618).dimensions()

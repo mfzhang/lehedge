ff4e81831c0795723f8581e7c5c0faced1e86a95
François Le Lay <mfworx@gmail.com>
2014-02-28 09:13:37 -0500

    swicth to iteration instead of recursion

--- goldendims.py
+++ goldendims.py
@@ -12,12 +12,16 @@ class GoldenRectangle:
         self.minRatio = 1
         self.maxRatio = s
 
-
     def dimensions(self):
+        oneSide = self.give_me_one_side()
+        otherSide = int(self.s/oneSide)
+        return (oneSide,otherSide)
+
+    def give_me_one_side(self):
         x0 = int(sqrt(self.s * self.r))
         print "x0=%d" % x0
-        leftHit = self.leftSearch(x0)
-        rightHit = self.rightSearch(x0+1)
+        leftHit = self.left_search(x0)
+        rightHit = self.right_search(x0+1)
         if ( leftHit ==0 ):
             if( rightHit == 0):
                 return 0
@@ -44,21 +48,26 @@ class GoldenRectangle:
             else: return rightHit
 
 
-    def leftSearch(self, a):
+    def left_search(self, a):
         print "a = %d" % (a)
-        if ((a*a)/self.s >= self.minRatio ):
-            if( notAnInteger(self.s/a) ):
-                return self.leftSearch(a-1)
-            else: return a
-        else: return 0
+        lowerBound = 0
+        while a > lowerBound :
+            if ((a*a)/self.s >= self.minRatio ):
+                if( notAnInteger(self.s/a) ):
+                    a = a - 1
+                else: break
+            else: return 0
+        return a
 
-
-    def rightSearch(self, a):
+    def right_search(self, a):
         print "a = %d" % (a)
-        if( (a*a)/self.s <= self.maxRatio ):
-            if ( notAnInteger(self.s/a) ):
-                return self.rightSearch(a+1)
-            else: return a
-        else: return 0
+        upperBound = self.s
+        while a < upperBound:
+            if( (a*a)/self.s <= self.maxRatio ):
+                if ( notAnInteger(self.s/a) ):
+                    a = a + 1
+                else: break
+            else: return 0
+        return a
 
-GoldenRectangle(1884).dimensions()
+GoldenRectangle(1904).dimensions()

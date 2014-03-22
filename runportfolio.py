31e9ada2e59a37d7178ae51fdcf1205f88e671ff
François Le Lay <mfworx@gmail.com>
2014-03-22 15:00:07 -0400

    add test datasets

--- runportfolio.py
+++ runportfolio.py
@@ -6,19 +6,20 @@ import portfolio
 
 p = portfolio.Portfolio()
 
-#ask = {"./$AUDUSD.Ask.txt":10000,
-#       "./$EURUSD.Ask.txt":10000,
-#       "./$USDJPY.Ask.txt":100}
+askFull = {"./$AUDUSD.Ask.txt":10000,
+       "./$EURUSD.Ask.txt":10000,
+       "./$USDJPY.Ask.txt":100}
 
-ask = {"./AUDUSD.Ask.Test.txt":10000,
+askTest = {"./AUDUSD.Ask.Test.txt":10000,
        "./EURUSD.Ask.Test.txt":10000,
        "./USDJPY.Ask.Test.txt":100}
+
+ask = askFull
 
 for (f,tick_res) in ask.iteritems():
     p.add_currency(f,tick_res)
 
 p.load_data()
-#p.align_start_times()
 p.compute_maximum_forward_size()
 p.compute_minimum_series_length()
 p.set_common_window_size()
@@ -27,5 +28,4 @@ p.clean_data()
 p.build_timeline()
 p.eat('training')
 p.sigmoidize('training')
-#p.cluster_profit_triples()
 p.plot_working_set_sample('training')

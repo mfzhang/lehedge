e33ee7c63cc0c45c07b74319c91ec6721fff26ec
François Le Lay <mfworx@gmail.com>
2014-08-13 14:17:14 -0400

    add tick data downloadr and headr

---
+++ download_data.R
@@ -0,0 +1,7 @@
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/EURJPY-Ask-0622-0813.txt",destfile="EURJPY-Ask.txt",method="curl")
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/EURJPY-Bid-0622-0813.txt",destfile="EURJPY-Bid.txt",method="curl")
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/EURUSD-Ask-0622-0813.txt",destfile="EURUSD-Ask.txt",method="curl")
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/EURUSD-Bid-0622-0813.txt",destfile="EURUSD-Bid.txt",method="curl")
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/USDJPY-Ask-0622-0813.txt",destfile="USDJPY-Ask.txt",method="curl")
+download.file("https://s3.amazonaws.com/crossgradient.com/fx/data/USDJPY-Bid-0622-0813.txt",destfile="USDJPY-Bid.txt",method="curl")
+

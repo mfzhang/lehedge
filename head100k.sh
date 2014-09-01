833ce487510eefe0882265eb23e9b14bbc6d3ab0
Ubuntu <mfworx@gmail.com>
2014-09-01 23:53:08 +0000

    clean up old stuff

--- head100k.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-head -n 100000 EURJPY-Ask.txt > EURJPY-Ask-100k.txt
-head -n 100000 EURJPY-Bid.txt > EURJPY-Bid-100k.txt
-head -n 100000 EURUSD-Ask.txt > EURUSD-Ask-100k.txt
-head -n 100000 EURUSD-Bid.txt > EURUSD-Bid-100k.txt
-head -n 100000 USDJPY-Ask.txt > USDJPY-Ask-100k.txt
-head -n 100000 USDJPY-Bid.txt > USDJPY-Bid-100k.txt
-
--- load_all_binary.R
+++
@@ -1,6 +0,0 @@
-source('common/munging_functions.R')
-
-EURUSD.ticks <- load_from_binary_light('EURUSD',100000)
-USDJPY.ticks <- load_from_binary_light('USDJPY',1000)
-EURJPY.ticks <- load_from_binary_light('EURJPY',1000)
-

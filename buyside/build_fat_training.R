d72f24d489f0abd61e4612f19ddb9f382297004d
François Le Lay <mfworx@gmail.com>
2014-09-06 10:42:28 -0400

    cleanup

--- buyside/build_fat_training.R
+++ buyside/build_fat_training.R
@@ -1,15 +1,15 @@
-
+# size of entire dataset
 nData <- nrow(all.rates)
 
-desiredSampleCount <- 1200000
-averageSkip <- round(nrow(all.rates)/desiredSampleCount)
-baseSkip <- 2*averageSkip
+desiredSampleCount <- 1500000
+samplingRate <- round(nrow(all.rates)/desiredSampleCount,2)
 
 # what we learn from
 backwardWindow <- 2*maxBestBuyForwardWindow
 forwardWindow  <- maxBestBuyForwardWindow
 
-# crop dataset to generous number of ticks to compute starting point
+# identify margins to avoid tick depletion at initialization and end phase of sampling
+
 all.rates.100k.head <- all.rates[1:100000,]
 
 EURUSD.prefill <- findMinRow(all.rates.100k.head, "EURUSD", backwardWindow)
@@ -18,11 +18,6 @@ USDJPY.prefill <- findMinRow(all.rates.100k.head, "USDJPY", backwardWindow)
 
 all.rates.100k.tail <- all.rates[(nData-99999):nData,]
 
-head(all.rates.100k.tail)
-nrow(all.rates.100k.tail)
-
-all.rates.100k.tail[nrow(all.rates.100k.tail):1400,"EURUSD.ask"]
-
 EURUSD.postfill <- findMaxRow(all.rates.100k.tail, "EURUSD", forwardWindow)
 EURJPY.postfill <- findMaxRow(all.rates.100k.tail, "EURJPY", forwardWindow)
 USDJPY.postfill <- findMaxRow(all.rates.100k.tail, "USDJPY", forwardWindow)
@@ -30,14 +25,17 @@ USDJPY.postfill <- findMaxRow(all.rates.100k.tail, "USDJPY", forwardWindow)
 # this gives us the first row of the dataset for which 
 # we know we have enough ticks for all 3 currencies to fill the backward window
 # it is where we should initialize the head of the tick reader
+
 minRow <- max(EURUSD.prefill,EURJPY.prefill,USDJPY.prefill)
 maxRow <- nData-max(EURUSD.postfill,EURJPY.postfill,USDJPY.postfill)+1
+
 print(paste("head initialization row = ",minRow,sep=""))
 
 # let the sampling happen!!
 # this will generate 1.2M images, about 4.7GB when backward window is 2600 pixels
 # returns a vector of zeros and ones indicating the end points of time frames for all samples
-samplingIdx <- sort(sample(minRow:maxRow,replace=FALSE,size=round(0.05*nData)))
+
+samplingIdx <- sort(sample(minRow:maxRow,replace=FALSE,size=round(samplingRate*nData)))
 stuff <- build_training_set_images("ask",backwardWindow,"buyside/img/",samplingIdx)
 
 # our looping adds one extra head, let's crop this 
